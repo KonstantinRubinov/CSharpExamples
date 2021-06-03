9e98e100643453df08e2ce58c3d0b89a705153f8
Konstantin Rubinov <Konstantin.Rubinov@gmail.com>
2021-06-03 11:22:09 +0300

    Add files via upload

---
+++ MessageEncoder/MessageEncoder/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace MessageEncoder
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string a = Encrypt("AaA");
+			string b = Encrypt("ZzZ");
+			string c = Encrypt("CiPhEr");
+
+			string d = Decrypt("BbB");
+			string e = Decrypt("AaA");
+			string f = Decrypt("DjQiFs");
+
+			string g = Decrypt("DjQiFs");
+		}
+
+		private const string Key1 = "BCDEFGHIJKLMNOPQRSTUVWXYZA";
+		private static string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		public static string Encrypt(string message, string key="")
+        {
+			StringBuilder stringBuilder = new StringBuilder();
+			foreach (char c in message)
+			{
+				bool isNotCapital = Char.IsLower(c);
+				int index = chars.ToUpper().IndexOf(c.ToString().ToUpper());
+				char ch = Key1[index];
+				if (isNotCapital == true)
+				{
+					ch = Char.ToLower(ch);
+
+				}
+				stringBuilder.Append(ch);
+			}
+			return stringBuilder.ToString();
+		}
+
+        public static string Decrypt(string message, string key="")
+        {
+			StringBuilder stringBuilder = new StringBuilder();
+			foreach (char c in message)
+			{
+				bool isNotCapital = Char.IsLower(c);
+				int index = Key1.ToUpper().IndexOf(c.ToString().ToUpper());
+				char ch = chars[index];
+				if (isNotCapital == true)
+				{
+					ch = Char.ToLower(ch);
+
+				}
+				stringBuilder.Append(ch);
+			}
+			return stringBuilder.ToString();
+		}
+	}
+}
